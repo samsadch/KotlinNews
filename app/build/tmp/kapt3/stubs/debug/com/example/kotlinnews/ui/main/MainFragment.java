051d7da56d1b6623703a4c4bf47acee5825cdffd
samsadcv <samsad@tnmonlinesolutions.com>
2019-11-11 21:30:23 +0400

    Added internet permission

--- app/build/tmp/kapt3/stubs/debug/com/example/kotlinnews/ui/main/MainFragment.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/kotlinnews/ui/main/MainFragment.java
@@ -2,11 +2,14 @@ package com.example.kotlinnews.ui.main;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u0000 \r2\u00020\u0001:\u0001\rB\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\b2\b\u0010\t\u001a\u0004\u0018\u00010\n2\b\u0010\u000b\u001a\u0004\u0018\u00010\fH\u0016R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000e"}, d2 = {"Lcom/example/kotlinnews/ui/main/MainFragment;", "Landroidx/fragment/app/Fragment;", "()V", "viewModel", "Lcom/example/kotlinnews/ui/main/MainViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\t\u001a\u00020\n2\u0006\u0010\u000b\u001a\u00020\f2\b\u0010\r\u001a\u0004\u0018\u00010\u000e2\b\u0010\u000f\u001a\u0004\u0018\u00010\u0010H\u0016R\u001b\u0010\u0003\u001a\u00020\u00048BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006\u00a8\u0006\u0011"}, d2 = {"Lcom/example/kotlinnews/ui/main/MainFragment;", "Landroidx/fragment/app/Fragment;", "()V", "viewModel", "Lcom/example/kotlinnews/ui/main/MainViewModel;", "getViewModel", "()Lcom/example/kotlinnews/ui/main/MainViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
 public final class MainFragment extends androidx.fragment.app.Fragment {
-    private com.example.kotlinnews.ui.main.MainViewModel viewModel;
-    public static final com.example.kotlinnews.ui.main.MainFragment.Companion Companion = null;
+    private final kotlin.Lazy viewModel$delegate = null;
     private java.util.HashMap _$_findViewCache;
+    
+    private final com.example.kotlinnews.ui.main.MainViewModel getViewModel() {
+        return null;
+    }
     
     @org.jetbrains.annotations.NotNull()
     @java.lang.Override()
@@ -19,18 +22,5 @@ public final class MainFragment extends androidx.fragment.app.Fragment {
     
     public MainFragment() {
         super();
-    }
-    
-    @kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u0003\u001a\u00020\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/example/kotlinnews/ui/main/MainFragment$Companion;", "", "()V", "newInstance", "Lcom/example/kotlinnews/ui/main/MainFragment;", "app_debug"})
-    public static final class Companion {
-        
-        @org.jetbrains.annotations.NotNull()
-        public final com.example.kotlinnews.ui.main.MainFragment newInstance() {
-            return null;
-        }
-        
-        private Companion() {
-            super();
-        }
     }
 }
